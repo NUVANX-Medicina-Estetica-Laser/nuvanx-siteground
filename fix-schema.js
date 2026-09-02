3c4fd79498143f38c5ead866d5f8382cc3844a39
Jeninefer Deras <jenineferderas@hotmail.com>
2026-09-02 13:20:19 +0200

    chore(repo): remove one-off schema rewrite utility

--- fix-schema.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('wp-content/themes/nuvanx-medical/inc/nvx-aesthetic-treatment-schema.php', 'utf8');
-content = content.replace(/if \( true \) \{\s*\}/, '$numeric_price = min( $prices );\n\t\t\t\t$high_price = max( $prices );');
-fs.writeFileSync('wp-content/themes/nuvanx-medical/inc/nvx-aesthetic-treatment-schema.php', content);
